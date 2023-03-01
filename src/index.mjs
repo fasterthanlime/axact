329a3d6412bf7f8f25973b659ac4ed8556b4e58d
Amos Wenger <amoswenger@gmail.com>
2023-03-01 19:51:36 +0100

    use the new websocket endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -18,15 +18,26 @@ function App(props) {
 
 let i = 0;
 
-let update = async () => {
-  let response = await fetch("/api/cpus");
-  if (response.status !== 200) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+// let update = async () => {
+//   let response = await fetch("/api/cpus");
+//   if (response.status !== 200) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   }
 
-  let json = await response.json();
+//   let json = await response.json();
+//   render(html`<${App} cpus=${json}></${App}>`, document.body);
+// };
+
+// update();
+// setInterval(update, 200);
+
+let url = new URL("/realtime/cpus", window.location.href);
+// http => ws
+// https => wss
+url.protocol = url.protocol.replace("http", "ws");
+
+let ws = new WebSocket(url.href);
+ws.onmessage = (ev) => {
+  let json = JSON.parse(ev.data);
   render(html`<${App} cpus=${json}></${App}>`, document.body);
 };
-
-update();
-setInterval(update, 200);

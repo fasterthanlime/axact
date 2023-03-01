14eb4c0d5fdab17890becc99516ab05bb276156d
Amos Wenger <amoswenger@gmail.com>
2023-03-01 18:35:59 +0100

    load and serve HTML from disk

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::sync::{Arc, Mutex};
 
-use axum::{extract::State, response::IntoResponse, routing::get, Json, Router, Server};
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router, Server,
+};
 use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
@@ -24,8 +29,11 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn root_get() -> &'static str {
-    "Hello"
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
+
+    Html(markup)
 }
 
 #[axum::debug_handler]

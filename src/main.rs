866d32b8db7b91961b0f073c1f11a2f8b9532b0a
Amos Wenger <amoswenger@gmail.com>
2023-03-01 18:27:09 +0100

    Add sysinfo, make / return state of all CPUs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use axum::{routing::get, Router, Server};
+use std::sync::{Arc, Mutex};
+
+use axum::{extract::State, routing::get, Router, Server};
+use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
+    let router = Router::new()
+        .route("/", get(root_get))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new())),
+        });
 
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
@@ -11,6 +18,25 @@ async fn main() {
     server.await.unwrap();
 }
 
-async fn root_get() -> &'static str {
-    "Hi from Axum!"
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
+
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu(); // Refreshing CPU information.
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {i} {usage}%").unwrap();
+    }
+
+    s
 }
